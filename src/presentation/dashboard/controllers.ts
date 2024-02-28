086c4219e1ea33b0caa93433a432bcc1413126f5
Joel Hernandez <you@example.com>
2024-02-28 05:52:25 -0500

    update data structure of  getTotalApplicationByFrequency endpoint

--- src/presentation/dashboard/controllers.ts
+++ src/presentation/dashboard/controllers.ts
@@ -15,10 +15,8 @@ export class DashboardController {
     getTotalApplicationByFrequency = async (req:Request,res:Response) =>{
         // TODO: Need to configure to return real data
         const {userId,frequency} = req.body;
-        
-        console.log({userId,frequency});
 
-        if(frequency === 'day'){
+        if(frequency === 'Day'){
           return res.status(200).json(
             [
               {
@@ -37,246 +35,131 @@ export class DashboardController {
           )
         }
 
-        if(frequency==='week'){
+        if(frequency==='Week'){
         
-          const weekResult = {
-            'Monday':[{
-              name: 'Application Sent',
-              total: 34,
+          const weekResult =[
+            {
+              date: 'Monday',
+              'Application Sent': 167,
+              'Interview': 145,
+              'Rejected': 135
             },
             {
-              name: 'Interviews',
-              total:2,
+              date: 'Tuesday',
+              'Application Sent': 125,
+              'Interview': 110,
+              'Rejected': 155,
             },
             {
-             name: 'Rejected',
-             total: 20,
-            }],
-            'Tuesday':[{
-              name: 'Application Sent',
-              total: 20,
+              date: 'Wednesday',
+              'Application Sent': 125,
+              'Interview': 110,
+              'Rejected': 155,
             },
             {
-              name: 'Interviews',
-              total:0,
+              date: 'Thursday',
+              'Application Sent': 125,
+              'Interview': 110,
+              'Rejected': 155,
             },
             {
-             name: 'Rejected',
-             total: 5,
-            }],
-            'Wednesday':[{
-              name: 'Application Sent',
-              total: 10,
+              date: 'Friday',
+              'Application Sent': 125,
+              'Interview': 110,
+              'Rejected': 155,
             },
             {
-              name: 'Interviews',
-              total:1,
+              date: 'Saturday',
+              'Application Sent': 125,
+              'Interview': 110,
+              'Rejected': 155,
             },
             {
-             name: 'Rejected',
-             total: 0,
-            }],
-            'Thursday':[{
-              name: 'Application Sent',
-              total: 15,
-            },
-            {
-              name: 'Interviews',
-              total:3,
-            },
-            {
-             name: 'Rejected',
-             total: 3,
-            }],
-            'Friday':[{
-              name: 'Application Sent',
-              total: 22,
-            },
-            {
-              name: 'Interviews',
-              total:1,
-            },
-            {
-             name: 'Rejected',
-             total: 4,
-            }],
-            'Saturday':[{
-              name: 'Application Sent',
-              total: 10,
-            },
-            {
-              name: 'Interviews',
-              total:0,
-            },
-            {
-             name: 'Rejected',
-             total: 1,
-            }],
-            'Sunday':[{
-              name: 'Application Sent',
-              total: 0,
-            },
-            {
-              name: 'Interviews',
-              total:0,
-            },
-            {
-             name: 'Rejected',
-             total: 0,
-            }],
-
-          }
+              date: 'Sunday',
+              'Application Sent': 125,
+              'Interview': 110,
+              'Rejected': 155,
+            }]
+            
            return res.status(200).json(weekResult);
-        }
+          }
+          
 
-        if(frequency ==='month'){
-          const monthResult = {
-          'January':[{
-            name: 'Application Sent',
-            total: 34,
-          },
-          {
-            name: 'Interviews',
-            total:2,
-          },
-          {
-           name: 'Rejected',
-           total: 20,
-          }],
-          'February':[{
-            name: 'Application Sent',
-            total: 20,
-          },
-          {
-            name: 'Interviews',
-            total:0,
-          },
-          {
-           name: 'Rejected',
-           total: 5,
-          }],
-          'March':[{
-            name: 'Application Sent',
-            total: 10,
-          },
-          {
-            name: 'Interviews',
-            total:1,
-          },
-          {
-           name: 'Rejected',
-           total: 0,
-          }],
-          'April':[{
-            name: 'Application Sent',
-            total: 15,
-          },
-          {
-            name: 'Interviews',
-            total:3,
-          },
-          {
-           name: 'Rejected',
-           total: 3,
-          }],
-          'May':[{
-            name: 'Application Sent',
-            total: 22,
-          },
-          {
-            name: 'Interviews',
-            total:1,
-          },
-          {
-           name: 'Rejected',
-           total: 4,
-          }],
-          'June':[{
-            name: 'Application Sent',
-            total: 10,
-          },
-          {
-            name: 'Interviews',
-            total:0,
-          },
-          {
-           name: 'Rejected',
-           total: 1,
-          }],
-          'July':[{
-            name: 'Application Sent',
-            total: 0,
-          },
-          {
-            name: 'Interviews',
-            total:0,
-          },
-          {
-           name: 'Rejected',
-           total: 0,
-          }],
-          'August':[{
-            name: 'Application Sent',
-            total: 0,
-          },
-          {
-            name: 'Interviews',
-            total:0,
-          },
-          {
-           name: 'Rejected',
-           total: 0,
-          }],
-          'September':[{
-            name: 'Application Sent',
-            total: 0,
-          },
-          {
-            name: 'Interviews',
-            total:0,
-          },
-          {
-           name: 'Rejected',
-           total: 0,
-          }],
-          'Octuber':[{
-            name: 'Application Sent',
-            total: 0,
-          },
-          {
-            name: 'Interviews',
-            total:0,
-          },
-          {
-           name: 'Rejected',
-           total: 0,
-          }],
-          'November':[{
-            name: 'Application Sent',
-            total: 0,
-          },
-          {
-            name: 'Interviews',
-            total:0,
-          },
-          {
-           name: 'Rejected',
-           total: 0,
-          }],
-
-          'December':[{
-            name: 'Application Sent',
-            total: 0,
-          },
-          {
-            name: 'Interviews',
-            total:0,
-          },
-          {
-           name: 'Rejected',
-           total: 0,
-          }],
-        }
+        if(frequency ==='Month'){
+          const monthResult = [
+            {
+              date: 'Jan 24',
+              'Application Sent': 300,
+              'Interview': 40,
+              'Rejected': 155,
+            },
+            {
+              date: 'Feb 24',
+              'Application Sent': 340,
+              'Interview': 140,
+              'Rejected': 200,
+            },
+            {
+              date: 'Mar 24',
+              'Application Sent': 400,
+              'Interview': 30,
+              'Rejected': 370,
+            },
+            {
+              date: 'Apr 24',
+              'Application Sent': 125,
+              'Interview': 25,
+              'Rejected': 100,
+            },
+            {
+              date: 'May 24',
+              'Application Sent': 200,
+              'Interview': 40,
+              'Rejected': 160,
+            },
+            {
+              date: 'Jun 24',
+              'Application Sent': 500,
+              'Interview': 110,
+              'Rejected': 155,
+            },
+            {
+              date: 'Jul 24',
+              'Application Sent': 135,
+              'Interview': 10,
+              'Rejected': 125,
+            },
+            {
+              date: 'Aug 24',
+              'Application Sent': 125,
+              'Interview': 110,
+              'Rejected': 155,
+            },
+            {
+              date: 'Sep 24',
+              'Application Sent': 189,
+              'Interview': 13,
+              'Rejected': 110,
+            },
+            {
+              date: 'Oct 24',
+              'Application Sent': 125,
+              'Interview': 110,
+              'Rejected': 155,
+            },
+            {
+              date: 'Nov 24',
+              'Application Sent': 155,
+              'Interview': 50,
+              'Rejected': 105,
+            },
+            {
+              date: 'Dec 24',
+              'Application Sent': 35,
+              'Interview': 5,
+              'Rejected': 30,
+            },
+          ];
 
         return res.status(200).json(monthResult);
     }
